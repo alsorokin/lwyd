6e6f7a04858c594b54010ff90040925ae063033c
Alexander Sorokin <alexander.sorokin@akvelon.com>
2017-03-01 20:42:42 +0300

    Grassy repeated tiles

---
+++ Look What You've Done/Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    static int levelWidth = 1000;
+    static int levelHeight = 1000;
+
+    public GameObject grassPrefab;
+
+    //Sprite[,] levelTiles = new Sprite[levelWidth, levelHeight];
+
+	// Use this for initialization
+	void Start () {
+        Quaternion zeroRotation = new Quaternion();
+        float tileWidth = grassPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+        float tileHeight = grassPrefab.GetComponent<SpriteRenderer>().bounds.size.y;
+        for (int w = 0; w < levelWidth; w++)
+        {
+            for (int h = 0; h < levelHeight; h++)
+            {
+                float x = w * tileWidth;
+                float y = h * tileHeight;
+                Instantiate(grassPrefab, new Vector3(x, y, 2), zeroRotation);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Look What You've Done/Assets/Test/TestPlayermoveScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayermoveScript : MonoBehaviour {
+    public float speed = 6.0F;
+    private Vector2 moveDirection = Vector2.zero;
+
+    void FixedUpdate()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+
+        rb.AddForce(moveDirection);
+        //.Move(moveDirection * Time.deltaTime);
+    }
+}
