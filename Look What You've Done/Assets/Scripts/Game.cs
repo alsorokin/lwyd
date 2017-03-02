27f59ccf9e7ce86bc844f0dc075525cd37bfd11b
Alexander Sorokin <alexander.sorokin@akvelon.com>
2017-03-02 22:22:09 +0300

    Fixed tile filtering and alignment

--- Look What You've Done/Assets/Scripts/Game.cs
+++ Look What You've Done/Assets/Scripts/Game.cs
@@ -8,20 +8,22 @@ public class Game : MonoBehaviour {
 
     public GameObject grassPrefab;
 
-    //Sprite[,] levelTiles = new Sprite[levelWidth, levelHeight];
+    GameObject[,] levelTiles = new GameObject[levelWidth, levelHeight];
 
 	// Use this for initialization
 	void Start () {
         Quaternion zeroRotation = new Quaternion();
         float tileWidth = grassPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
         float tileHeight = grassPrefab.GetComponent<SpriteRenderer>().bounds.size.y;
+        float offsetX = levelWidth / 2;
+        float offsetY = levelHeight / 2;
         for (int w = 0; w < levelWidth; w++)
         {
             for (int h = 0; h < levelHeight; h++)
             {
-                float x = w * tileWidth;
-                float y = h * tileHeight;
-                Instantiate(grassPrefab, new Vector3(x, y, 2), zeroRotation);
+                float x = (w - offsetX) / 4;
+                float y = (h - offsetY) / 4;
+                levelTiles[h, w] = Instantiate(grassPrefab, new Vector3(x, y, 2), zeroRotation);
             }
         }
 	}
