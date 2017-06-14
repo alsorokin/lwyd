502a873747e661f8b229e69e6ee638558168fc72
Alexander Sorokin <alexander.sorokin@akvelon.com>
2017-06-14 14:54:32 +0300

    Better input handling -> more fluent player movement!

--- Look What You've Done/Assets/Scripts/MovementController.cs
+++ Look What You've Done/Assets/Scripts/MovementController.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public enum Direction : sbyte
 {
-    None=0, Down=1, Bottom=1, Right=2, Up=3, Top=3, Left=4
+    None = 0, Down = 1, Bottom = 1, Right = 2, Up = 3, Top = 3, Left = 4
 }
 
-public class MovementController : MonoBehaviour {
+public class MovementController : MonoBehaviour
+{
     public float movementSpeed = 100;
     public Direction direction = Direction.Up;
+    public bool isMoving = false;
 
-    private bool isMoving = false;
     private Vector3 stopPosition;
+    private Direction whereToNext = Direction.None;
 
     // Use this for initialization
-    void Start () {}
-    
+    void Start() { }
+
     // Update is called once per frame
-    void Update () {}
+    void Update() { }
 
     void FixedUpdate()
     {
@@ -28,65 +30,100 @@ public class MovementController : MonoBehaviour {
             {
                 case Direction.Down:
                     transform.position += new Vector3(0, -GetMovementScalar(movementSpeed), 0);
-                    if (transform.position.y <= stopPosition.y)  {
-                        isMoving = false;
-                        transform.position = stopPosition;
+                    if (transform.position.y <= stopPosition.y)
+                    {
+                        StopMoving();
                     }
                     break;
                 case Direction.Up:
                     transform.position += new Vector3(0, GetMovementScalar(movementSpeed), 0);
-                    if (transform.position.y >= stopPosition.y)  {
-                        isMoving = false;
-                        transform.position = stopPosition;
+                    if (transform.position.y >= stopPosition.y)
+                    {
+                        StopMoving();
                     }
                     break;
                 case Direction.Left:
                     transform.position += new Vector3(-GetMovementScalar(movementSpeed), 0, 0);
-                    if (transform.position.x <= stopPosition.x)  {
-                        isMoving = false;
-                        transform.position = stopPosition;
+                    if (transform.position.x <= stopPosition.x)
+                    {
+                        StopMoving();
                     }
                     break;
                 case Direction.Right:
                     transform.position += new Vector3(GetMovementScalar(movementSpeed), 0, 0);
-                    if (transform.position.x >= stopPosition.x)  {
-                        isMoving = false;
-                        transform.position = stopPosition;
+                    if (transform.position.x >= stopPosition.x)
+                    {
+                        StopMoving();
                     }
                     break;
             }
         }
     }
 
-    public void GoUp() {
-        if (!isMoving) {
+    private void StopMoving()
+    {
+        // align to grid
+        transform.position = stopPosition;
+        // only actually stop if no further commands were issued
+        isMoving = whereToNext != Direction.None;
+        // then start moving again, if user already issued a new command
+        direction = whereToNext;
+        whereToNext = Direction.None;
+    }
+
+    public void GoUp()
+    {
+        if (!isMoving)
+        {
             isMoving = true;
             direction = Direction.Up;
             stopPosition = transform.position + new Vector3(0, 1, 0);
         }
+        else
+        {
+            whereToNext = Direction.Up;
+        }
     }
 
-    public void GoDown() {
-        if (!isMoving) {
+    public void GoDown()
+    {
+        if (!isMoving)
+        {
             isMoving = true;
             direction = Direction.Down;
             stopPosition = transform.position + new Vector3(0, -1, 0);
         }
+        else
+        {
+            whereToNext = Direction.Down;
+        }
     }
 
-    public void GoLeft() {
-        if (!isMoving) {
+    public void GoLeft()
+    {
+        if (!isMoving)
+        {
             isMoving = true;
             direction = Direction.Left;
             stopPosition = transform.position + new Vector3(-1, 0, 0);
         }
+        else
+        {
+            whereToNext = Direction.Left;
+        }
     }
 
-    public void GoRight() {
-        if (!isMoving) {
+    public void GoRight()
+    {
+        if (!isMoving)
+        {
             isMoving = true;
             direction = Direction.Right;
             stopPosition = transform.position + new Vector3(1, 0, 0);
+        }
+        else
+        {
+            whereToNext = Direction.Right;
         }
     }
 
@@ -97,7 +134,8 @@ public class MovementController : MonoBehaviour {
 
     void OnGUI()
     {
-        if (Application.isEditor) {
+        if (Application.isEditor)
+        {
             GUI.Label(new Rect(new Vector2(10, 50), new Vector2(550, 20)), "isMoving: " + isMoving.ToString());
             GUI.Label(new Rect(new Vector2(10, 70), new Vector2(550, 20)), "stopPosition" + stopPosition.ToString());
         }
--- Look What You've Done/Assets/Test/TestPlayermoveScript.cs
+++ Look What You've Done/Assets/Test/TestPlayermoveScript.cs
@@ -2,29 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPlayermoveScript : MonoBehaviour {
+public class TestPlayermoveScript : MonoBehaviour
+{
+
+    private bool sentUp = false;
+    private bool sentDown = false;
+    private bool sentLeft = false;
+    private bool sentRight = false;
 
     void FixedUpdate()
     {
         MovementController mc = GetComponent<MovementController>();
+        if (!mc.isMoving)
+        {
+            sentUp = false;
+            sentDown = false;
+            sentLeft = false;
+            sentRight = false;
+        }
 
-        if (Input.GetAxis("Horizontal") > 0.1) {
-            mc.GoRight();
+        if (Input.GetAxis("Horizontal") > 0.001)
+        {
+            if (!sentRight)
+            {
+                mc.GoRight();
+                sentRight = true;
+            }
         }
-        if (Input.GetAxis("Horizontal") < -0.1) {
-            mc.GoLeft();
+        else
+        {
+            sentRight = false;
         }
-        if (Input.GetAxis("Vertical") > 0.1) {
-            mc.GoUp();
+        if (Input.GetAxis("Horizontal") < -0.001)
+        {
+            if (!sentLeft)
+            {
+                mc.GoLeft();
+                sentLeft = true;
+            }
+
         }
-        if (Input.GetAxis("Vertical") < -0.1) {
-            mc.GoDown();
+        else
+        {
+            sentLeft = false;
         }
-    } 
+        if (Input.GetAxis("Vertical") > 0.001)
+        {
+            if (!sentUp)
+            {
+                mc.GoUp();
+                sentUp = true;
+            }
+        }
+        else
+        {
+            sentUp = false;
+        }
+        if (Input.GetAxis("Vertical") < -0.001)
+        {
+            if (!sentDown)
+            {
+                mc.GoDown();
+                sentDown = true;
+            }
+        }
+        else
+        {
+            sentDown = false;
+        }
+    }
 
     void OnGUI()
     {
-        if (Application.isEditor) {
+        if (Application.isEditor)
+        {
             GUI.Label(new Rect(new Vector2(10, 10), new Vector2(50, 20)), Input.GetAxis("Horizontal").ToString());
             GUI.Label(new Rect(new Vector2(10, 30), new Vector2(50, 20)), Input.GetAxis("Vertical").ToString());
         }
